37ea08410ab00791acab546769df7376402b471d
Elena <uo258525@uniovi.es>
2019-03-21 13:20:32 +0100

    SDI-NodeJS- 8,9,10 Recursos estáticos, Peticiones POST y parámetros, Enrutamiento y comodines

--- app.js
+++ app.js
@@ -2,14 +2,24 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+
+//meter body parser dentro de express
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 // Variables
 app.set('port', 8081);
+
+//antes de mirar controladores mirar public lo primero
+app.use(express.static('public'));
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
 require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
 
 // lanzar el servidor
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log("Servidor activo");
 })
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,6 +1,6 @@
 //crea un modulo
 module.exports = function (app) {
-
+    //tener claro si hay que usar re.query, reg.params, reg.body
     app.get('/canciones/:id', function (req, res) {
         var respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
@@ -10,6 +10,13 @@ module.exports = function (app) {
             + 'Genero: ' + req.params.genero;
         res.send(respuesta);
     });
+
+    app.post("/cancion", function(req, res) {
+        res.send("Canción agregada:"+req.body.nombre +"<br>"
+            +" genero :" +req.body.genero +"<br>"
+            +" precio: "+req.body.precio);
+    });
+
 
 }
 
