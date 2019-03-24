4b43f8463201eb46fea85cc71aef125def9b8887
Elena <uo258525@uniovi.es>
2019-03-24 23:19:05 +0100

    SDI-NodeJS-8.4-Subida de ficheros y gestion de colecciones”

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express');
 var app = express();
 
 var swig = require('swig');
-
+var fileUpload = require('express-fileupload');
+app.use(fileUpload());
 var mongo = require('mongodb');
+
+
 
 var bodyParser = require('body-parser');
 //meter body parser dentro de express
@@ -17,7 +20,7 @@ gestorBD.init(app,mongo);
 
 // Variables
 app.set('port', 8081);
-app.set('db', ' mongodb://admin:sdi@tiendamusica-shard-00-00-so4wa.mongodb.net:27017,tiendamusica-shard-00-01-so4wa.mongodb.net:27017,tiendamusica-shard-00-02-so4wa.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00-so4wa.mongodb.net:27017,tiendamusica-shard-00-01-so4wa.mongodb.net:27017,tiendamusica-shard-00-02-so4wa.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
 
 //antes de mirar controladores mirar public lo primero
 app.use(express.static('public'));
--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -34,6 +34,39 @@ module.exports = function (app, swig, gestorBD) {
         res.send(respuesta);
     });
 
+    app.get("/tienda", function (req, res) {
+        var criterio = {};
+        if (req.query.busqueda != null) {
+            criterio = {"nombre": {$regex: ".*" + req.query.busqueda + ".*"}};
+        }
+        gestorBD.obtenerCanciones(criterio, function (canciones) {
+            if (canciones == null) {
+                res.send("Error al listar ");
+            } else {
+                var respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        canciones: canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
+    app.get('/cancion/:id', function (req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)};
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.send(respuesta);
+            } else {
+                var respuesta = swig.renderFile('views/bcancion.html',
+                    {
+                        cancion : canciones[0]
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
+
 
     app.post("/cancion", function (req, res) {
         var cancion = {
@@ -45,9 +78,28 @@ module.exports = function (app, swig, gestorBD) {
         // Conectarse
         gestorBD.insertarCancion(cancion, function (id) {
             if (id == null) {
-                res.send("Error al insertar canción");
+                res.send("Error al insertar ");
             } else {
-                res.send("Agregada la canción ID: " + id);
+
+                if (req.files.portada != null) {
+                    var imagen = req.files.portada;
+                    imagen.mv('public/portadas/' + id + '.png', function (err) {
+                        if (err) {
+                            res.send("Error al subir la portada");
+                        } else {
+                            if (req.files.audio != null) {
+                                var audio = req.files.audio;
+                                audio.mv('public/audios/' + id + '.mp3', function (err) {
+                                    if (err) {
+                                        res.send("Error al subir el audio");
+                                    } else {
+                                        res.send("Agregada id: " + id);
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
             }
         });
     });
