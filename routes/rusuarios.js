914652d625f97fb616ac3e312a29f084bf11bddf
Elena <uo258525@uniovi.es>
2019-03-31 18:02:35 +0200

    SDI-NodeJS-9.4- Mensajes y alertas

--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -24,10 +24,10 @@ module.exports = function (app, swig, gestorBD) {
                 }
                 gestorBD.insertarUsuario(usuario, function (id) {
                     if (id == null) {
-                        res.send("Error al insertar ");
+                        res.redirect("/registrarse?mensaje=Error al registrar usuario");
                     } else {
                         //res.send('Usuario Insertado ' + id);
-                        res.redirect("/identificarse");
+                        res.redirect("/identificarse?mensaje=Nuevo usuario registrado");
                     }
 
                 });
@@ -72,7 +72,9 @@ module.exports = function (app, swig, gestorBD) {
         gestorBD.obtenerUsuarios(criterio, function (usuarios) {
             if (usuarios == null || usuarios.length == 0) {
                 req.session.usuario = null;
-                res.send("No identificado: ");
+                res.redirect("/identificarse" +
+                    "?mensaje=Email o password incorrecto"+
+                    "&tipoMensaje=alert-danger ");
             } else {
                 req.session.usuario = usuarios[0].email;
                 res.redirect("/publicaciones");
